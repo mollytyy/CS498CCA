36e4f9d50fcfaa95241234807cade3d15ab7790e
mollytyy <moty0519@gmail.com>
2023-05-03 01:03:32 -0500

    server

--- MP12_PublicFiles/server.py
+++ MP12_PublicFiles/server.py
@@ -1,40 +1,64 @@
 from kubernetes import client, config
-from flask import Flask,request
+from flask import Flask, request
 from os import path
-import yaml, random, string, json
+import yaml
+import random
+import string
+import json
 import sys
 import json
 
 # Configs can be set in Configuration class directly or using helper utility
 config.load_kube_config()
 v1 = client.CoreV1Api()
+batch_v1 = client.BatchV1Api()
 app = Flask(__name__)
 # app.run(debug = True)
+
 
 @app.route('/config', methods=['GET'])
 def get_config():
     pods = []
 
-    # your code here
+    list_pod = v1.list_pod_for_all_namespaces().items
+
+    for pod in list_pod:
+        d = {
+            "name": pod.metadata.name,
+            "ip": pod.status.pod_ip,
+            "namespace": pod.metadata.namespace,
+            "node": pod.spec.node_name,
+            "status": pod.status.phase
+        }
+        pods.append(d)
 
     output = {"pods": pods}
     output = json.dumps(output)
 
     return output
 
-@app.route('/img-classification/free',methods=['POST'])
+
+@app.route('/img-classification/free', methods=['POST'])
 def post_free():
-    # your code here
+    free = open("free-job.yaml")
+    job = yaml.safe_load(free)
+
+    api_response = batch_v1.create_namespaced_job(
+        body=job, namespace="free-service")
 
     return "success"
 
 
 @app.route('/img-classification/premium', methods=['POST'])
 def post_premium():
-    # your code here
+    premium = open("premium-job.yaml")
+    job = yaml.safe_load(premium)
+
+    api_response = batch_v1.create_namespaced_job(
+        body=job, namespace="default")
 
     return "success"
 
-    
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0', port=5000)
